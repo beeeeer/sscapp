38da05b42b2dbef7ddab7fa8743eb7aff77eed8c
Adam Nowak <adamcbf@gmail.com>
2015-05-30 20:12:12 +0200

    women size convert added

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
 
  
     @IBOutlet weak var mensShoeSizeTextField: UITextField!
-    
     @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    @IBOutlet weak var WomensConvertedSizeLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -34,11 +35,20 @@ class ViewController: UIViewController {
     let conversionConstant = 30
         mensConvertedShoeSizeLabel.hidden = false
         mensConvertedShoeSizeLabel.text = "\(sizeFromTextfield + conversionConstant)" + " in European shoe size"
+        mensConvertedShoeSizeLabel.textColor = UIColor.blueColor()
         
-        
-      
-    
     }
 
+    @IBAction func convertWomenShoeSizeButtonPressed(sender: UIButton) {
+        
+        
+        let sizeFromTextField = Double((womensShoeSizeTextField.text as NSString).doubleValue)
+        
+        let conversionConstant = 30.5
+        
+        WomensConvertedSizeLabel.hidden = false
+        WomensConvertedSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+        WomensConvertedSizeLabel.textColor = UIColor.redColor()
+    }
 }
 
